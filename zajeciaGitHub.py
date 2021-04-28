4cd6a152bef2e1ddac584f244fc0b534f7448f64
aloboda9 <83343399+aloboda9@users.noreply.github.com>
2021-04-28 12:48:17 +0200

    Update zajeciaGitHub.py

--- zajeciaGitHub.py
+++ zajeciaGitHub.py
@@ -6,9 +6,11 @@ def dysjunkcja(wartosc_pierwsza, wartosc_druga):
         return "True"
 
 for i in range(4):
-    print("Podaj wartość pierwszego elementu (1 = True, 0 = False).")
+    print("Podaj wartość pierwszego elementu (1 = True, 0 = False).") 
     wartosc_pierwsza = int(input())
     print("Podaj wartość drugiego elementu (1 = True, 0 = False).")
     wartosc_druga = int(input())
     wynik = dysjunkcja(wartosc_pierwsza, wartosc_druga)
     print("Podane elementy złączone spójnikiem dysjunkcji tworzą zdanie o wartości:", wynik)
+   
+#dodam komentarz bo nawet nie bardzo wiem co mogę zmienić w tym kodzie
